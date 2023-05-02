f3396776afdb17c4a785c3a703a2cf69c0b4d2ee
calum <calum.quinn@heig-vd.ch>
2023-05-02 16:03:01 +0200

    Fini

--- src/ArrayDeque.h
+++ src/ArrayDeque.h
@@ -15,7 +15,7 @@ public:
    using pointer = T*;
    using size_type = std::size_t;
 
-   ArrayDeque(size_type _capacite = 0) : debut(0),
+   explicit ArrayDeque(size_type _capacite = 0) : debut(0),
                                          taille(0),
                                          capacite(_capacite),
                                          buffer(nullptr) {
@@ -26,23 +26,61 @@ public:
 
    // Placez ici les méthodes publiques nécessaires pour passer le codecheck
 
-   size_type i_physique(size_type i_logique) const {
-      return (debut + i_logique + capacite) % capacite;
+   /**
+   * @name            : ArrayDeque
+   *
+   * @but             : Constructeur par copie
+   *
+   * @param other     : L'objet à copier
+   * @return          : Un objet de type ArrayDeque
+   * @throws          : NIL
+   */
+   ArrayDeque(const ArrayDeque& other) : ArrayDeque(other.capacite){
+      for (; taille < other.taille; ++taille) {
+         new(buffer + taille) value_type(other[taille]);
+      }
    }
 
+   /**
+     * @name            : operator[]
+     *
+     * @but             : opérateur pour accéder à un élément du buffer
+     *
+     * @param i         : emplacement de l'élément
+     * @return          : référence sur l'élément
+     * @throws          : NIL
+     */
    reference operator[](size_type i) {
       return buffer[i_physique(i)];
    }
 
+
+   /**
+     * @name            : operator[]
+     *
+     * @but             : opérateur pour accéder à un élément du buffer
+     *
+     * @param i         : emplacement de l'élément
+     * @return          : une référence constante sur l'élément
+     * @throws          : NIL
+     */
    const_reference operator[](size_type i) const {
       return buffer[i_physique(i)];
    }
 
-   ArrayDeque& operator=(const ArrayDeque& other) {
-      capacite = other.capacite;
-
-      for (; taille < other.taille; ++taille) {
-         new(buffer + taille) value_type(other[taille]);
+   /**
+     * @name            : operator=
+     *
+     * @but             : opérateur d'affectation
+     *
+     * @param other     : buffer à affecter
+     * @return          : référence sur le buffer à retourner
+     * @throws          : NIL
+     */
+   ArrayDeque& operator=(const ArrayDeque& other){
+      //Si une affectation sur lui même est faite, il retourne lui même sans faire les opérations
+      if (this == &other) {
+         return *this;
       }
    }
 
@@ -58,11 +96,30 @@ public:
       ++taille;
    }
 
+
+   /**
+     * @name            : push_back
+     *
+     * @but             : ajoute un élément à la fin du buffer
+     *
+     * @param i         : élément à ajouter
+     * @return          : void
+     * @throws          : NIL
+     */
    void push_back(value_type i) {
       new(buffer + i_physique(taille)) value_type (i);
       ++taille;
    }
 
+
+    /**
+     * @name            : pop_front
+     *
+     * @but             : supprimer le premier élément
+     *
+     * @return          : void
+     * @throws          : NIL
+     */
    void pop_front() {
       std::destroy_at(&buffer[i_physique(0)]);
       ++debut;
@@ -76,18 +133,33 @@ public:
       }
    }
 
+
+   /**
+     * @name            : front
+     *
+     * @but             : retourne le premier élément
+     *
+     * @return          : rertourne une référence sur le premier élément
+     * @throws          : NIL
+     */
    reference front() const {
-      return buffer[debut];
+      return buffer[i_physique(0)];
    }
 
    reference back() const {
       return buffer[i_physique(taille - 1)];
    }
 
-   reference  back() {
-      return *(buffer + i_physique(taille - 1));
-   }
 
+   /**
+     * @name            : swap
+     *
+     * @but             : échanger deux buffers
+     *
+     * @param other     : buffer avec lequel échanger
+     * @return          : void
+     * @throws          : NIL
+     */
    void swap(ArrayDeque& other) {
       std::swap(capacite,other.capacite);
       std::swap(taille,other.taille);
@@ -107,9 +179,18 @@ public:
    }
 
    void shrink_to_fit() {
-
+      changer_capacite(true);
    }
 
+
+   /**
+   * @name            :  ~ArrayDeque
+   *
+   * @but             : déstructeur de l'objet ArrayDeque
+   *
+   * @return          : void
+   * @throws          : NIL
+   */
    ~ArrayDeque() {
       for (size_type i = 0; i < taille; ++i) {
          std::destroy_at(&buffer[i_physique(i)]);
@@ -122,34 +203,59 @@ public:
 
 private:
    pointer buffer;
-   size_type capacite;
-   size_type debut;
-   size_type taille;
+   size_type capacite{};
+   size_type debut{};
+   size_type taille{};
 
 
    // Placez ici les méthodes privées qui permettent de rendre votre code plus claiar
 
+   /**
+     * @name            : changer_capacite
+     *
+     * @but             : agrandir ou rétrécir la capacité du buffer
+     *
+     * @param shrink    : booléen qui définit si on rétréci
+     * @return          : void
+     * @throws          : NIL
+     */
+   void changer_capacite(bool shrink = false) {
+      size_type nouvelleCapacite;
+      //Si on veut diminuer la capacite
+      if (shrink) {
+         nouvelleCapacite = taille;
+      }
+      //Si on veut augmenter la capacite
+      else {
+         nouvelleCapacite = capacite == 0 ? 1 : capacite * 2;
+      }
+      ArrayDeque<value_type> tmp(nouvelleCapacite);
+      for (size_type i = 0; i < taille; ++i) {
+         new(tmp.buffer + i) value_type(std::move(buffer[i_physique(i)]));
+         ++tmp.taille;
+      }
+
+      std::swap(buffer, tmp.buffer);
+      std::swap(capacite, tmp.capacite);
+      std::swap(debut, tmp.debut);
+   }
 
 
-
+   /**
+  * @name            : i_physique
+  *
+  * @but             : fonction servant à récupérer l'emplacement physique d'un élément dans le buffer
+  *
+  * @param i_logique : indice logique de l'élément dans le buffer
+  * @return          : size_t
+  * @throws          : NIL
+  */
+   size_type i_physique(size_type i_logique) const {
+      //Retourne l'emplacement physique d'un élément
+      return (debut + i_logique + capacite) % capacite;
+   }
 
 };
-/*
-template <typename T> inline
-std::ostream& operator<<(std::ostream& s,
-                         const ArrayDeque<T>& v) {
-   s << "(" << v.size() << "/" << v.capacity() << ") : ";
-   if (not v.empty())
-      s << v.front() << " -> " << v.back() << " : ";
-   s << "[";
-   for (int i = 0; i < v.size(); ++i) {
-      s << *(v + i);
-      if (i != v.size() - 1)
-         s << ", ";
-   }
-   s << "]";
-   return s;
-}*/
 
 template <typename T> inline
 std::ostream& operator<<(std::ostream& s,
@@ -167,4 +273,4 @@ std::ostream& operator<<(std::ostream& s,
    return s;
 }
 
-#endif /* ArrayDeque_hpp */
+#endif //ArrayDeque_h
